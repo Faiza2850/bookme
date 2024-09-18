9007a273623804a6c931b80056b37950fbb0f7e3
Faiza2850 <saffafatima06@gmail.com>
2024-09-18 19:38:07 +0500

    last commit

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -11,7 +11,7 @@ import { FaArrowRight, FaCalendarPlus, FaCreditCard, FaPencilAlt } from 'react-i
 export const Profile = () => {
   return (
    
-    <div className='p-10  xl:p-20 xl:pr-96 md:p-8 sm:max-w-3xl max-w-5xl bg-white mx-auto  rounded-xl'>
+    <div className='p-10  xl:p-20 xl:pr-40 md:p-8 sm:max-w-3xl max-w-2xl bg-white mx-auto  rounded-xl'>
         
         <h1 className='font-bold text-2xl '>Bookme</h1>
         <h5 className=''>Profile</h5>
@@ -26,8 +26,8 @@ export const Profile = () => {
         <h4 className='pt-5 font-semibold'>Manage Your Bookings </h4>
         <p className='text-[#667085] pb-5'>You can make amendments to your bookings up until the 24 hours before your Journey is due to start. You can also downlload copies of receipts for previous bookings here.</p>
         <div className='flex gap-6 '>
-        <button className='bg-[#FFCA09] text-black p-2 rounded py-3 font-semibold flex w-60 items-center justify-center '>Make a Booking <FaCalendarPlus className="mr-2" /></button>
-        <button className='bg-[#FFCA09] text-black p-2 rounded py-3 font-semibold flex w-60 items-center justify-center '>View Bookings <img src={viewbook} className='mr-2' /></button>
+        <button className='bg-[#FFCA09] text-black p-2 rounded py-3 font-semibold flex w-60 items-center justify-center '>Make a Booking <FaCalendarPlus className="mr-2 ml-2" /></button>
+        <button className='bg-[#FFCA09] text-black p-2 rounded py-3 font-semibold flex w-60 items-center justify-center '>View Bookings <img src={viewbook} className='mr-2 ml-2' /></button>
         </div>
 
         <h4 className='pt-5 font-semibold'>Recover Your Quotes</h4>
@@ -43,8 +43,8 @@ remove them here.</p>
         <p className='text-[#667085] pb-5'>Keeping your details Up to date helps us to contact you in the event of 
         a problem with your booking. You may Also Change your Password here.</p>
         <div className='flex gap-6'>
-        <button className='bg-[#FFCA09] text-black p-2 rounded py-3 font-semibold flex w-60 items-center justify-center '>Log Out <img src={logout} className='mr-2' /> </button>
-        <button className='bg-[#FFCA09] text-black p-2 rounded py-3 font-semibold flex w-60 items-center justify-center '>Edit Profile <FaPencilAlt className="mr-2 " /> </button>
+        <button className='bg-[#FFCA09] text-black p-2 rounded py-3 font-semibold flex w-60 items-center justify-center '>Log Out <img src={logout} className='mr-2 ml-2' /> </button>
+        <button className='bg-[#FFCA09] text-black p-2 rounded py-3 font-semibold flex w-60 items-center justify-center '>Edit Profile <FaPencilAlt className="mr-2 ml-2 " /> </button>
         </div>
     </div>
   )
