cd9f51aa79dac7ad7ef8175b34f6a27e03617947
Faiza2850 <saffafatima06@gmail.com>
2024-09-20 19:06:44 +0500

    latest

--- src/components/Submission.jsx
+++ src/components/Submission.jsx
@@ -5,18 +5,17 @@ import check from '../images/check.svg';
 export const Submission = () => {
   return (
    <div className='sm:pt-2 lg:pt-8 p-16 rounded mx-auto'>
-    <h1 className='font-bold text-xl '>
+    <h1 className='font-bold text-lg sm:text-xl  '>
         BOOKMEE
     </h1>
     <h4 className='mb-4' >Checkout Form</h4>
-    <div className='px-12 pt-6 lg:px-28 center justify-center  text-center shadow-2xl border-black border-2 rounded-xl lg:mx-96'>
-   <img src={check} className='mr-2  ml-20 lg:ml-36  mb-4 '/>
+    <div className='px-12 sm:px-12  md:px-20 lg:mx-80 md:justify-center pt-6 lg:px-28 center justify-center  text-center shadow-2xl border-black border-2 rounded-xl lg:w- md:w-1/2 '>
+   <img src={check} className='mr-2 sm:flex sm:justify-center ml-10 lg:ml-44 md:justify-center  mb-4  w-12 sm:w-16'/>
         <h2 className='font-semibold'>Booking Completed</h2>
         <p className='text-[#667085]'>Your Booking has been Completed For
         Airport Taxi.</p>
         <div className="flex justify-center mt-6 mb-8 ">
-            <button className="bg-black text-[#FEB601] px-6
-             p-2 text-xs rounded flex w-84 items-center justify-center mr-4">
+            <button className="bg-black text-[#FEB601] px-6 p-2 text-xs rounded flex w-84 items-center justify-center mr-4">
               Done
              
             </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {
       boxShadow: {
         'custom': '3px 2px 4.1px 0px #00000091',
